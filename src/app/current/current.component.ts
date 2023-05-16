cb4743746972162dcedec58de9eec0ac73d903ab
Rangu Sushma <rangu.sushmma2610@gmail.com>
2023-05-16 12:09:58 +0530

    first Coomit

---
+++ src/app/current/current.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
+import { CurrentWeather } from '../current-weather';
+
+
+@Component({
+  selector: 'app-current',
+  templateUrl: './current.component.html',
+  styleUrls: ['./current.component.css']
+})
+export class CurrentComponent implements OnInit{
+  myWeather: CurrentWeather | undefined;
+  constructor(private ws:WeatherService){ }
+  ngOnInit(){
+    this.myWeather = this.ws.weatherNow();
+  }
+
+}
